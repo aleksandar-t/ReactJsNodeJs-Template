85e4e5a3b0f9c4e2ebac0d7c4837c2c1c396de94
Aleksandar Tomovski <aleksandar.tomovski@gmail.com>
2015-03-17 23:42:42 +0100

    react node template project

---
+++ www/js/myPage.js
@@ -0,0 +1,12 @@
+var MyPage = React.createClass({
+    render: function () {
+        return (
+            <div className="page">                
+                <div className="content">
+                    ererererere
+                </div>
+            </div>
+        );
+    }
+});
+
